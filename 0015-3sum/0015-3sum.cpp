92dfd684d1a79f0519a6f9cf06cac99adb44ae3b
Asmi Agarwal <asmi.agarwal2006@gmail.com>
2026-08-17 19:00:41 +0530

    Time: 51 ms (42.35%), Space: 29.1 MB (46.73%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,49 +1,54 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector < vector < int >> v ; 
+        int n = nums.size();
 
-        vector < vector < int > > v;  
+        sort(nums.begin() , nums.end());
+        for ( int i =0 ; i < nums.size(); i ++){
+            if ( i > 0 && nums[i]== nums[i-1]){
+                continue ; 
+            }
+            int j = i +1;
+            int k = n-1;
+            while ( j < k ){
+                int sum= nums[i]+nums[j]+nums[k];
 
-        sort( nums.begin() , nums.end());
-        int n= nums.size();
-
-
-        for ( int i =0 ; i < nums.size();i++){
-            if (i > 0 && nums[i] == nums[i - 1]) {
-         continue;
-         }
-            int j = i+1;
-            int k=n-1;
-            while ( j < k  ){
-                int sum = nums[i]+nums[j]+nums[k];
-
-                if ( sum == 0){
-                    v.push_back({nums[i], nums[j], nums[k]});
-                    j++;
-                    k--;
-
-                    while ( j < k  && nums[j]==nums[j-1]){
+                if (sum==0){
+                    while ( j < k && nums [j]==nums[j+1]){
                         j++;
                     }
-                    while( j < k  && nums[k]==nums[k+1] ){
+                    while ( j < k && nums[k]==nums[k-1]){
                         k--;
                     }
 
-            
+                    v.push_back({nums[i], nums[j], nums[k]});
+
+                    j++;
+                    k--;
                 }
-                else if ( sum < 0 ){
-                    j++;}
-                else{
-                        k--;
-                    }
-                
-                
-                
 
+                else if ( sum < 0 ){
+                    j++;
+
+                }
+                else{
+                    
+                    k--;
+                }
+                
             }
 
         }
-        return v; 
+
+                    return v; 
+
+
+        
+
+
+
+
         
     }
 };
