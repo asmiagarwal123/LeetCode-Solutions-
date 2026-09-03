d7a27d4ea618bebf65b58c4b5b5aa871bc81170a
Asmi Agarwal <asmi.agarwal2006@gmail.com>
2026-09-04 00:22:20 +0530

    Time: 3 ms (50.9%), Space: 23.9 MB (49.54%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,25 +2,62 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
 
-        vector<int> ls;
-        map<int, int> mpp;
-
         int n = nums.size();
-        int minimum = n / 3 + 1;
 
+        int candidate1 = 0;
+        int candidate2 = 0;
+
+        int count1 = 0;
+        int count2 = 0;
+
+        // First pass: find possible candidates
         for (int i = 0; i < n; i++) {
 
-            mpp[nums[i]]++;
-
-            if (mpp[nums[i]] == minimum) {
-                ls.push_back(nums[i]);
+            if (nums[i] == candidate1) {
+                count1++;
             }
-
-            if (ls.size() == 2) {
-                break;
+            else if (nums[i] == candidate2) {
+                count2++;
+            }
+            else if (count1 == 0) {
+                candidate1 = nums[i];
+                count1 = 1;
+            }
+            else if (count2 == 0) {
+                candidate2 = nums[i];
+                count2 = 1;
+            }
+            else {
+                count1--;
+                count2--;
             }
         }
 
-        return ls;
+        // Second pass: count actual occurrences
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+
+            if (num == candidate1) {
+                count1++;
+            }
+            else if (num == candidate2) {
+                count2++;
+            }
+        }
+
+        vector<int> ans;
+
+        // Verify candidates
+        if (count1 > n / 3) {
+            ans.push_back(candidate1);
+        }
+
+        if (count2 > n / 3) {
+            ans.push_back(candidate2);
+        }
+
+        return ans;
     }
 };
